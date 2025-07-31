8ac9b77949c13689e8a5901c7cbf15ccb4813c9c
Rachit Khandelwal <160627704+rachitkhandelwal41@users.noreply.github.com>
2025-07-31 21:55:41 +0530

    Update index.js

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -10,6 +10,6 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/api/v1", routes_1.default);
-app.listen(443, () => {
-    console.log("Server running on http://localhost:443");
+app.listen(8080, () => {
+    console.log("Server running on http://localhost:8080");
 });
