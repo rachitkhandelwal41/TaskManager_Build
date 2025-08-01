cd71a0acd32845b02a2d589cca8653848d665698
Rachit Khandelwal <160627704+rachitkhandelwal41@users.noreply.github.com>
2025-08-01 13:11:05 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -9,6 +9,6 @@ app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
-app.listen(443, () => {
-  console.log("Server running on http://localhost:443");
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
